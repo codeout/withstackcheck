60f0fde982cf944678aa9e55c2144a93cc67af09
Shintaro Kojima <goodies@codeout.net>
2023-12-02 13:05:56 +0900

    chore: skeleton github.com/codeout/withstackcheck

---
+++ withstackcheck_test.go
@@ -0,0 +1,15 @@
+package withstackcheck_test
+
+import (
+	"testing"
+
+	"github.com/codeout/withstackcheck"
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, withstackcheck.Analyzer, "a")
+}
